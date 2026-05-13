8de0eec4e96f3bb220fe246dc386f950e3ec095e
meharsai12 <meharsairavikanti@gmail.com>
2026-05-13 12:17:53 +0530

    Roboshop ec2 instances script

---
+++ mongodb.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+USERID = $(id -u)
+
+R="\e[31m"  #red colour
+G="\e[32m"  #green colour
+Y="\e[33m"  #yellow colour
+N="\e[0m"   #no colour
+
+LOGS_FOLDER ="/var/logs/roshop-logs"
+SCRIPT_NAME =$(echo $0  | cut -d "." -f1 ) 
+LOG_FILE = "$LOGS_FOLDER/$SCRIPT_NAME.log"
+
+
+mkdir -p $LOGS_FOLDER    # if we add -p if we run n times ifd folder ewxistis no creation if not it will create 
+
+ echo "Script started  Executing at time ::  $(date)"
+
+
+ if [ $USERID -ne 0 ]
+ then 
+  echo  -e " You don't have root access ,  $R Please run the script with root access $N "
+ else
+
+  echo -e " You have root access $G you can perform actions $N"
+
+  fi
