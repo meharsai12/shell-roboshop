720df34b93fb80a26001b2fa68b248d9931e9f11
meharsai12 <meharsairavikanti@gmail.com>
2026-05-13 12:21:02 +0530

    mongodb instance creation

--- mongodb.sh
+++ mongodb.sh
@@ -20,6 +20,7 @@ mkdir -p $LOGS_FOLDER    # if we add -p if we run n times ifd folder ewxistis no
  if [ $USERID -ne 0 ]
  then 
   echo  -e " You don't have root access ,  $R Please run the script with root access $N "
+  exit 1
  else
 
   echo -e " You have root access $G you can perform actions $N"
