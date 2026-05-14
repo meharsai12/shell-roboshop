05b11cc2dfcf3fbf8a82ee713c946f09105dd696
meharsai12 <meharsairavikanti@gmail.com>
2026-05-14 19:26:41 +0530

    rabbitmq

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -11,19 +11,20 @@ LOGS_FOLDER="/var/logs/roshop-logs"
 SCRIPT_NAME=$(echo $0  | cut -d "." -f1 ) 
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 SCRIPT_DIR=$PWD
+START_TIME=$(date +%s)
 
 mkdir -p $LOGS_FOLDER    # if we add -p if we run n times ifd folder ewxistis no creation if not it will create 
 
- echo "Script started  Executing at time ::  $(date)"
+ echo "Script started  Executing at time ::  $(date)"  | tee -a $LOG_FILE
 
 
  if [ $USERID -ne 0 ]
  then 
-  echo  -e " You don't have root access ,  $R Please run the script with root access $N "
+  echo  -e " You don't have root access ,  $R Please run the script with root access $N "  | tee -a $LOG_FILE
   exit 1
  else
 
-  echo -e " You have root access $G you can perform actions $N"
+  echo -e " You have root access $G you can perform actions $N"  | tee -a $LOG_FILE
 
   fi
 
@@ -34,26 +35,33 @@ read -s MYSQL_ROOT_PASSWORD
 VALIDATE(){
     if [ $1 -eq 0 ]
     then 
-    echo  -e "$2  is :: ... $G  success  $N"
+    echo  -e "$2  is :: ... $G  success  $N"  | tee -a $LOG_FILE
     else
-    echo -e  "$2 .. ::  $R is failure  $N  "
+    echo -e  "$2 .. ::  $R is failure  $N  "  | tee -a $LOG_FILE
     exit 1
 
     fi
 }
 
 
-cp $SCRIPT_DIR/rabbitmq.repo  /etc/yum.repos.d/rabbitmq.repo
+cp $SCRIPT_DIR/rabbitmq.repo  /etc/yum.repos.d/rabbitmq.repo  &>>$LOG_FILE
 VALIDATE $? "copying rabbit mq repo "
 
-dnf install rabbitmq-server -y 
+dnf install rabbitmq-server -y  &>>$LOG_FILE
 VALIDATE $? "Installing rabbitmq server"
 
-systemctl enable rabbitmq-server 
+systemctl enable rabbitmq-server  &>>$LOG_FILE
 VALIDATE $? "Enabling rabbitmq server"
 
 systemctl start rabbitmq-server 
 VALIDATE $? "Starting rabbitmq server"
 
-rabbitmqctl add_user roboshop $RABBITMQ_PASSWD 
+rabbitmqctl add_user roboshop $RABBITMQ_PASSWD  &>>$LOG_FILE
 rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" 
+
+
+END_TIME=$(date +%s)
+
+TOTAL_TIME=$(($START_TIME - $END_TIME))
+
+echo -e "Total time taken to execute the script is $R $TOTAL_TIME seconds $N "
