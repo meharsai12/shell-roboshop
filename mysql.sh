aa5d7f03274d9b0232fb320579beb0b1c88146b1
meharsai12 <meharsairavikanti@gmail.com>
2026-05-14 19:17:39 +0530

    mysql

--- mysql.sh
+++ mysql.sh
@@ -11,19 +11,20 @@ LOGS_FOLDER="/var/logs/roshop-logs"
 SCRIPT_NAME=$(echo $0  | cut -d "." -f1 ) 
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 SCRIPT_DIR=$PWD
+START_TIME=$(date +%s)
 
 mkdir -p $LOGS_FOLDER    # if we add -p if we run n times ifd folder ewxistis no creation if not it will create 
 
- echo "Script started  Executing at time ::  $(date)"
+ echo "Script started  Executing at time ::  $(date)" | tee -a $LOG_FILE
 
 
  if [ $USERID -ne 0 ]
  then 
-  echo  -e " You don't have root access ,  $R Please run the script with root access $N "
+  echo  -e " You don't have root access ,  $R Please run the script with root access $N "  | tee -a $LOG_FILE
   exit 1
  else
 
-  echo -e " You have root access $G you can perform actions $N"
+  echo -e " You have root access $G you can perform actions $N"  | tee -a $LOG_FILE
 
   fi
 
@@ -34,21 +35,21 @@ read -s MYSQL_ROOT_PASSWORD
 VALIDATE(){
     if [ $1 -eq 0 ]
     then 
-    echo  -e "$2  is :: ... $G  success  $N"
+    echo  -e "$2  is :: ... $G  success  $N"   | tee -a $LOG_FILE
     else
-    echo -e  "$2 .. ::  $R is failure  $N  "
+    echo -e  "$2 .. ::  $R is failure  $N  "   | tee -a $LOG_FILE
     exit 1
 
     fi
 }
 
 
-dnf install mysql-server -y
+dnf install mysql-server -y  &>>$LOG_FILE
 VALIDATE $? "installing the mysql server"
 
-systemctl enable mysqld
+systemctl enable mysqld  &>>$LOG_FILE
 systemctl start mysqld  
 VALIDATE $? "enabling and starting the mysql server"
 
-mysql_secure_installation --set-root-pass $MYSQL_ROOT_PASSWORD 
+mysql_secure_installation --set-root-pass $MYSQL_ROOT_PASSWORD   &>>$LOG_FILE
 VALIDATE $? "Setting MySQL root password"
