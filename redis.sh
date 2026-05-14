fbd43760254eb9f01e6f73c9e2dccd4b0f89b428
meharsai12 <meharsairavikanti@gmail.com>
2026-05-14 19:10:33 +0530

    redis

--- redis.sh
+++ redis.sh
@@ -9,45 +9,51 @@ LOGS_FOLDER="/var/logs/roshop-logs"
 SCRIPT_NAME=$(echo $0  | cut -d "." -f1 ) 
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 SCRIPT_DIR=$PWD
+START_TIME=$(date +%s)
 
 mkdir -p $LOGS_FOLDER 
 
-echo -e "Script executed at .. ::  $G $(date) $N"
+echo -e "Script executed at .. ::  $G $(date) $N"  | tee -a $LOG_FILE| tee -a $LOG_FILE
 
 
 if [ USERID -ne 0 ]
 then 
-echo -e " $R You don't root access to perform  $N"
+echo -e " $R You don't root access to perform  $N"   | tee -a $LOG_FILE
 exit 1
 else
-echo -e " $G You have rooot access to perform actions $N"
+echo -e " $G You have rooot access to perform actions $N"  | tee -a $LOG_FILE
 fi
 
 
 VALIDATE(){
     if [ $1 -eq  0 ]
     then 
-    echo -e " $2 is .....  $Y success $N "
+    echo -e " $2 is .....  $Y success $N "  | tee -a $LOG_FILE
     else
-    echo -e " $2 is .....  $Y Failure $N "
+    echo -e " $2 is .....  $Y Failure $N "  | tee -a $LOG_FILE
     fi
 }
 
-dnf module disable redis -y
+dnf module disable redis -y  &>>$LOG_FILE
 VALIDATE $? "Diable default redis "
 
-dnf module enable redis:7 -y
+dnf module enable redis:7 -y    &>>$LOG_FILE
 VALIDATE $? "enabling redis:7 version"
 
-dnf install redis -y 
+dnf install redis -y    &>>$LOG_FILE
 VALIDATE $? "installing redis"
 
-sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf
+sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf   &>>$LOG_FILE
 VALIDATE $? "Edited redis.conf to accept remote connections"
 
-systemctl enable redis 
+systemctl enable redis  &>>$LOG_FILE
 VALIDATE $? "enabling redis"
 
-systemctl start redis 
+systemctl start redis   &>>$LOG_FILE
 VALIDATE $? "Starting redis"
 
+END_TIME=$(date +%s)
+
+TOTAL_TIME=$(($START_TIME - $END_TIME))
+
+echo -e "Total time taken to execute the script is $R $TOTAL_TIME seconds $N "
