62d1d90bb88cb64a224348556292707bb3f7b570
meharsai12 <meharsairavikanti@gmail.com>
2026-05-15 13:33:47 +0530

    user

--- user.sh
+++ user.sh
@@ -54,7 +54,7 @@ dnf install nodejs -y   &>>$LOG_FILE
 VALIDATE $? "installing nodejs"
 
 id roboshop
-if [ id -ne 0 ]
+if [ $? -ne 0 ]
 then
   useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop   &>>$LOG_FILE
   VALIDATE $? "Roboshop user creation "
