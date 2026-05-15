24c952c7a9919a3189f41da27e7b943a61da453c
meharsai12 <meharsairavikanti@gmail.com>
2026-05-15 13:34:09 +0530

    cart

--- cart.sh
+++ cart.sh
@@ -50,7 +50,7 @@ dnf install nodejs -y  &>>$LOG_FILE
 VALIDATE $? "installing the nodejs"
 
 id roboshop 
-if [ id -ne  0 ]
+if [ $? -ne  0 ]
 then
     useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop  &>>$LOG_FILE
     VALIDATe $? "Roboshop user crteation "
