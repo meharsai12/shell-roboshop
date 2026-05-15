15154db619be7b6c54dbe8331f2b762636536599
meharsai12 <meharsairavikanti@gmail.com>
2026-05-15 13:40:02 +0530

    shipping

--- shipping.sh
+++ shipping.sh
@@ -52,7 +52,7 @@ VALIDATE $? "installing  maven"
 
 
 id roboshop
-if [ id -ne 0 ]
+if [ $? -ne 0 ]
 then 
     useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop  &>>$LOG_FILE
     VALIDATE $? "Roboshop user creation "
